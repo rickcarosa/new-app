946e53f4c648bcfd6e2bd0a92fcdc23b01997c00
ricardocarosa <rickcarosa@yahoo.com>
2022-07-26 10:42:38 -0400

    add list and stats and form components

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Card from './shared/Card';
+
+const FeedbackForm = () => {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate our service with us?</h2>
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='Write a review'
+            value={text}
+            onChange={handleTextChange}
+          />
+          <button type='submit'>Send</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
